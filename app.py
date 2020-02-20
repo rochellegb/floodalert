cb89485842ff34691d6515d71807bebf37f96c77
rochellegb <rochelle_bastian22@yahoo.com>
2020-02-20 09:43:19 +0800

    Added get method for code

--- app.py
+++ app.py
@@ -10,15 +10,22 @@ app_key = os.environ.get('GLOBE_APP_SECRET')
 app_id = os.environ.get('GLOBE_APP_ID')
 
 
-@app.route('/store/')
-def get_item(name):
-    url = 'https://developer.globelabs.com.ph/oauth/access_token?app_id={}&app_secret={}&code={}'.format(app_id, app_key, )
-    items = []
+#missing redirect uri
+@app.route('/code/', methods=['GET'])
+def get_globe_code():
     queries = request.args
-    items.append(name)
-    print(queries.get("page"))
-    print(queries.get("size"))
-    return jsonify(items), 200
+    code = queries.get("code")
+    print(code)
+    return jsonify("success"), 200
+
+
+@app.route('/access_token/', methods=['POST'])
+def post_globe_details():
+    url = 'https://developer.globelabs.com.ph/oauth/access_token?app_id={}&app_secret={}&code={}'.format(app_id, app_key, '')
+    queries = request.args
+    print(queries.get("app_id"))
+    print(queries.get("app_secret"))
+    return jsonify(), 200
 
 
 app.run(port=5000)
