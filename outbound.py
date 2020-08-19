cb763b1820bfea564fa02dbd9e2ab687410b1d9f
rochellegb <rochelle_bastian22@yahoo.com>
2020-08-19 16:40:45 +0800

    Update outbound.py

--- outbound.py
+++ outbound.py
@@ -1,25 +1,24 @@
-import os
-from dotenv import load_dotenv
 import requests
 
-load_dotenv()
 
-short_code = os.environ.get('GLOBE_SHORT_CODE')
+def outbound(message, access_token, subscriber_number):
 
+    short_code = '7128'
+    access_token = access_token
+    address = subscriber_number
+    clientCorrelator = '21587128'
+    message = message
 
-shortcode = short_code[-4:]
-access_token = '1UAMW2nbi6l6gCw8wG_djbt_NiT7JSqwmyqhtsXUK2g'
-address = '9773005800'
-clientCorrelator = '268401'
-message = 'WAZZUP THIS IS GLOBE'
+    url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/"+short_code+"/requests"
 
-url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/" + shortcode + "/requests"
+    querystring = {"access_token": access_token}
 
-querystring = {"access_token": access_token}
+    payload = "{\"outboundSMSMessageRequest\": { \"clientCorrelator\": \""+clientCorrelator+"\", 
+                \"senderAddress\": \""+short_code+"\", 
+                \"outboundSMSTextMessage\": {\"message\": \""+message+"\"}, 
+                \"address\": \""+address+"\" } }"
+    headers = {'Content-Type': "application/json", 'Host': "devapi.globelabs.com.ph"}
 
-payload = "{\"outboundSMSMessageRequest\": { \"clientCorrelator\": \"" + clientCorrelator + "\", \"senderAddress\": \"" + shortcode + "\", \"outboundSMSTextMessage\": {\"message\": \"" + message + "\"}, \"address\": \"" + address + "\" } }"
-headers = {'Content-Type': "application/json", 'Host': "devapi.globelabs.com.ph"}
+    response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
 
-response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
-
-print(response.text)
+    return response.text
