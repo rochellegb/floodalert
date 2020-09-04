e9c4cd923155bba1bb7017d364664444f892583a
rochellegb <rochelle_bastian22@yahoo.com>
2020-09-04 09:41:52 +0800

    Update announcements.py

--- Models/announcements.py
+++ Models/announcements.py
@@ -1,61 +1,25 @@
-import os
-from dotenv import load_dotenv
 from db import db
-from datetime import datetime
-import requests
-
-load_dotenv()
-
-short_code = os.environ.get('GLOBE_SHORT_CODE')
 
 
 class Announcements(db.Model):
     __tablename__ = "Announcements"
 
     id = db.Column(db.Integer, primary_key=True)
+    time_posted = db.Column(db.String(250))
     height = db.Column(db.Float())
     level = db.Column(db.Integer)
-    message = db.Column(db.String(250))
-    posted = db.Column(db.DateTime, default=datetime.now)
+    category_level = db.Column(db.String(250))
+    # message = db.Column(db.String(250))
 
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
 
-    #outbound every level
-    def message_to_many(self, access_token, sender_address, message):
-        shortcode = short_code[-4:]
-        access_token = access_token
-        address = sender_address
-        clientCorrelator = '268401'
-        message = message
+    def __str__(self):
+        return f'{self.id}: {self.height}, {self.level}, {self.time_posted}'
 
-        url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/" + shortcode + "/requests"
+    def __repr__(self):
+        return f'Announcements({self.id}: {self.height}, {self.level}, {self.time_posted})'
 
-        querystring = {"access_token": access_token}
 
-        payload = "{\"outboundSMSMessageRequest\": { \"clientCorrelator\": \"" + clientCorrelator + "\", \"senderAddress\": \"" + shortcode + "\", \"outboundSMSTextMessage\": {\"message\": \"" + message + "\"}, \"address\": \"" + address + "\" } }"
-        headers = {'Content-Type': "application/json", 'Host': "devapi.globelabs.com.ph"}
 
-        response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
-
-        print(response.text)
-
-    #outbound for update
-    def message_after_opt_in(self, access_token, subscriber_number):
-        shortcode = short_code[-4:]
-        access_token = access_token
-        address = subscriber_number
-        clientCorrelator = '268401'
-        message = "Hello"
-
-        url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/" + shortcode + "/requests"
-
-        querystring = {"access_token": access_token}
-
-        payload = "{\"outboundSMSMessageRequest\": { \"clientCorrelator\": \"" + clientCorrelator + "\", \"senderAddress\": \"" + shortcode + "\", \"outboundSMSTextMessage\": {\"message\": \"" + message + "\"}, \"address\": \"" + address + "\" } }"
-        headers = {'Content-Type': "application/json", 'Host': "devapi.globelabs.com.ph"}
-
-        response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
-
-        print(response.text)
