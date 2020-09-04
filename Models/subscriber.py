a0f000ef7fde36577de6103bbfce6278f43693ff
rochellegb <rochelle_bastian22@yahoo.com>
2020-09-04 09:42:24 +0800

    Update subscriber.py

--- Models/subscriber.py
+++ Models/subscriber.py
@@ -1,4 +1,11 @@
+import os
 from db import db
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+short_code = os.environ.get('GLOBE_SHORT_CODE')
 
 
 class Subscribers(db.Model):
@@ -16,3 +23,25 @@ class Subscribers(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+    @classmethod
+    def find_by_number(cls, subscriber_number):
+        return cls.query.filter_by(subscriber_number=subscriber_number).first()
+
+    #outbound every level
+    def message_to_many(self, message):
+        shortcode = short_code[-4:]
+        access_token = self.access_token
+        address = self.subscriber_number
+        clientCorrelator = '268401'
+        message = message
+
+        url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/" + shortcode + "/requests"
+
+        querystring = {"access_token": access_token}
+
+        payload = "{\"outboundSMSMessageRequest\": { \"clientCorrelator\": \"" + clientCorrelator + "\", \"senderAddress\": \"" + shortcode + "\", \"outboundSMSTextMessage\": {\"message\": \"" + message + "\"}, \"address\": \"" + address + "\" } }"
+        headers = {'Content-Type': "application/json", 'Host': "devapi.globelabs.com.ph"}
+
+        response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+
+        print(response.text)
